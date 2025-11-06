7feca3e7971b2353f982ccad1d964a238cf5d49c
Graham <graham.barnett@wintermute-trading.com>
2025-11-06 13:32:35 +0000

    recurse fix

--- yolo-rustc-bootstrap/src/lib.rs
+++ yolo-rustc-bootstrap/src/lib.rs
@@ -10,16 +10,13 @@ pub fn do_crimes(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
         )
         .expect("version failed");
         let version = version.split(' ').nth(1).unwrap();
-        let mut trick = !version.ends_with("-nightly");
+        let trick = !version.ends_with("-nightly");
 
         let mut args = std::env::args_os();
         let exe = args.next().expect("get first arg");
         let mut args: Vec<std::ffi::OsString> = args.collect();
 
         let rust_analyzer = exe.clone().into_string().unwrap().ends_with("rust-analyzer-proc-macro-srv");
-        if trick && rust_analyzer {
-            trick = false;
-        }
 
         if trick {
             println!("\x1b[1;32m   Hijacking\x1b[m this rustc process");
@@ -38,17 +35,8 @@ pub fn do_crimes(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
         let mut cmd = std::process::Command::new(&exe);
         let status = if rust_analyzer {
-            cmd.args(args).env("RUSTC_BOOTSTRAP", "1");
-
             println!("\x1b[1;32m    Skipping cmd: {:?}", cmd);
-
             return Default::default()
-        } else if !trick {
-            cmd.args(args).env("RUSTC_BOOTSTRAP", "1");
-
-            // println!("\x1b[1;32m    Running cmd: {:?}", cmd);
-
-            cmd.status().unwrap()
         } else {
             let mut insert_at = 0;
             for (idx, a) in args.iter().enumerate() {
@@ -62,18 +50,16 @@ pub fn do_crimes(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
             cmd.args(args).env("RUSTC_BOOTSTRAP", "1");
 
             println!("\x1b[1;32m    Running trick cmd: {:?}", cmd);
-
             cmd.status().unwrap()
         };
 
-        if trick {
-            if status.success() {
-                println!("\x1b[1;32m    Finished\x1b[m the dirty work");
-                println!("\x1b[1;32m      Hiding\x1b[m all the evidence");
-                println!("\x1b[1;32m  Continuing\x1b[m as if nothing happened");
-            } else {
-                println!("\x1b[1;33m    Finished cmd {:?} with error {:?}", std::env::args_os(), status);
-            }
+        if !status.success() {
+            println!("\x1b[1;33m    Finished cmd {:?} with error {:?}", std::env::args_os(), status);
+        }
+        else if trick && status.success() {
+            println!("\x1b[1;32m    Finished\x1b[m the dirty work");
+            println!("\x1b[1;32m      Hiding\x1b[m all the evidence");
+            println!("\x1b[1;32m  Continuing\x1b[m as if nothing happened");
         }
 
         std::process::exit(status.code().unwrap_or(101));
